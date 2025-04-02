f88c7e9d1922f03fe099e777a805c3f2264fe9f7
Ellie Alavi <elhamalavi.f@gmail.com>
2025-04-02 14:23:50 -0400

    Middle School Updated

--- tests/test_middle_school_student.py
+++ tests/test_middle_school_student.py
@@ -1,25 +1,62 @@
-# from school_schedule.middle_school_student import MiddleSchoolStudent
+from school_schedule.middle_school_student import MiddleSchoolStudent
 
-# def test_new_valid_middle_school_student_gets_transportation():
-#     # Arrange
-#     name = "Ellis"
-#     grade = "junior"
-#     classes = ["Painting"]
+def test_new_valid_middle_school_student_gets_transportation():
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
 
-#     # Act
-#     ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=True)
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=True)
 
-#     assert ellis.name == name
-#     assert ellis.grade == grade
-#     assert ellis.classes == classes
-#     assert len(ellis.classes) == 1
-#     assert ellis.gets_transportation
+    assert ellis.name == name
+    assert ellis.grade == grade
+    assert ellis.classes == classes
+    assert len(ellis.classes) == 1
+    assert ellis.gets_transportation
 
 def test_new_valid_middle_school_student_with_defaults():
-    pass
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+    
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes)
+
+    #Assertation
+    assert ellis.name == name
+    assert ellis.grade == grade
+    assert ellis.classes == classes
+    assert len(ellis.classes) == 1
+    assert not ellis.gets_transportation
+
 
 def test_middle_school_student_summary_with_transportation():
-    pass
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=True)
+
+    #Assert
+    summary = ellis.summary()
+
+    assert summary == "Ellis is a junior enrolled in 1 classes: Painting\nEllis gets transportation"
 
 def test_middle_school_student_summary_without_transportation():
-    pass
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+    
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes)
+
+    #Assertation
+    summary = ellis.summary()
+
+    assert summary == "Ellis is a junior enrolled in 1 classes: Painting\nEllis doesn't get transportation"
+    
